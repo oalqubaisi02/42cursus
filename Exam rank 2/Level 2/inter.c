fdee5c854840531f07b69a84c1075191b5ba178a
oalqubaisi02 <158767830+oalqubaisi02@users.noreply.github.com>
2024-07-15 10:13:57 +0400

    Add files via upload

---
+++ Exam rank 2/Level 2/inter.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    if (ac == 3)
+    {
+        int checker[256] = {0};
+        int i = 0;
+
+        while (av[2][i] != '\0')
+            checker[(int)av[2][i++]] = 1;
+        i = 0;
+        while (av[1][i] != '\0')
+        {
+            if (checker[(int)av[1][i]] == 1)
+            {
+                write(1, &av[1][i], 1);
+                checker[(int)av[1][i]] = 2;
+            }
+            i++;
+        }
+    }
+    write (1, "\n", 1);
+}
